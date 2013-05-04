cc9a95b95702db97b0f370dfff80fdb2336e7c69
Alexander Lebedev <lebedev@smartblobs.org>
2013-05-04 07:52:40 +0000

    type classes was removed from primitive types

--- PrimitiveTypes.hs
+++ PrimitiveTypes.hs
@@ -3,16 +3,9 @@
 
 module PrimitiveTypes where
 
-import Control.Arrow
-import Control.Category
-import Control.Applicative
-import Data.Monoid
-import Prelude hiding (id, (.))
-import Link
-import Data.Typeable
+-- import Prelude
 import Data.IORef
 import Control.Monad
-import Haste
 
 data Source a = Source (IORef [Sink a])
               | Merge (Source a) (Source a)
@@ -31,14 +24,14 @@ createSource call = do
 --   ref <- newIORef []
 --   let sink = Sink $ \v -> call $ \v' -> 
 
-delay :: Int -> IO (AsyncPipe a a)
-delay dt = do
-  ref <- newIORef []
-  let sink = Sink $ \v -> do
-        subs <- readIORef ref                    
-        setTimeout dt $ forM_ subs $ \c -> runSink c v
-      source = Source ref
-  return $ AsyncPipe sink source
+-- delay :: Int -> IO (AsyncPipe a a)
+-- delay dt = do
+--   ref <- newIORef []
+--   let sink = Sink $ \v -> do
+--         subs <- readIORef ref                    
+--         setTimeout dt $ forM_ subs $ \c -> runSink c v
+--       source = Source ref
+--   return $ Async sink source
   
 filterSource :: (a -> Bool) -> Source a -> IO (Source a)
 filterSource p source = do
@@ -48,104 +41,85 @@ filterSource p source = do
         case p v of
           False -> return ()
           True  -> forM_ subs $ \c -> runSink c v
-  source `link` sink
+  source `sourceSink` sink
   return $ Source ref
 
 newtype Sink a = Sink { runSink :: a -> IO () }
 
-newtype SyncPipe a b = SyncPipe { runSyncPipe :: a -> IO b }
+data Pipe a b = Async (Sink a) (Source b)
+              | Sync (a -> IO b)
 
-data AsyncPipe a b = AsyncPipe (Sink a) (Source b)
-                   | FromSync (SyncPipe a b)
+-- instance Category SyncPipe where
+--   id = SyncPipe $ runKleisli id
+--   (.) = flip link
+  
+-- instance Arrow SyncPipe where
+--   arr = SyncPipe . runKleisli . arr
+--   first = SyncPipe . runKleisli . first . Kleisli . runSyncPipe
 
-instance Category SyncPipe where
-  id = SyncPipe $ runKleisli id
-  (.) = flip link
+-- instance Functor (SyncPipe t) where
+--   fmap f p = arr f . p
   
-instance Arrow SyncPipe where
-  arr = SyncPipe . runKleisli . arr
-  first = SyncPipe . runKleisli . first . Kleisli . runSyncPipe
+-- instance Applicative (SyncPipe t) where
+--   pure = arr . const
+--   (<*>) fp vp = arr (\(f,v) -> f v) . (fp &&& vp)
 
-instance Functor (SyncPipe t) where
-  fmap f p = arr f . p
+-- instance Monoid a => Monoid (SyncPipe t a) where
+--   mempty = arr $ const mempty
+--   mappend p1 p2 = mappend <$> p1 <*> p2
   
-instance Applicative (SyncPipe t) where
-  pure = arr . const
-  (<*>) fp vp = arr (\(f,v) -> f v) . (fp &&& vp)
+-- instance Monoid (Sink a) where
+--   mempty = Sink $ \_ -> return ()
+--   mappend (Sink s1) (Sink s2) = Sink $ \v -> s1 v >> s2 v
+  
+sourceSink :: Source a -> Sink a -> IO ()
+sourceSink (Source ref) sink = modifyIORef ref (sink:)
+sourceSink (Merge s1 s2) sink = s1 `sourceSink` sink >> s2 `sourceSink` sink
+sourceSink EmptySource sink = return ()
 
-instance Monoid a => Monoid (SyncPipe t a) where
-  mempty = arr $ const mempty
-  mappend p1 p2 = mappend <$> p1 <*> p2
-  
-instance Monoid (Sink a) where
-  mempty = Sink $ \_ -> return ()
-  mappend (Sink s1) (Sink s2) = Sink $ \v -> s1 v >> s2 v
-  
-
-instance Link (Source a) (Sink a) where
-  type L (Source a) (Sink a) = IO ()
-  link (Source ref) sink = modifyIORef ref (sink:)
-  link (Merge s1 s2) sink = s1 `link` sink >> s2 `link` sink
-  link EmptySource sink = return ()
-  
-instance Link (Source a) (SyncPipe a b) where
-  type L (Source a) (SyncPipe a b) = IO (Source b)
-  link (Source ref) pipe = do
+sourcePipe :: Source a -> Pipe a b -> IO (Source b)
+sourcePipe source@(Source ref) pipe = case pipe of
+  Sync runPipe -> do
     ref1 <- newIORef []
     let sink = Sink $ \v -> do
           subs <- readIORef ref1
           case null subs of
             True -> return ()
             False -> do
-              v' <- runSyncPipe pipe v
+              v' <- runPipe v
               forM_ subs $ \c -> runSink c v'
     
     modifyIORef ref (sink:)
     return $ Source ref1
-  
-  link (Merge s1 s2) pipe = do
-    s1' <- s1 `link` pipe 
-    s2' <- s2 `link` pipe
-    return $ Merge s1' s2'
-  
-  link EmptySource pipe = return EmptySource
 
-instance Link (Source a) (AsyncPipe a b) where
-  type L (Source a) (AsyncPipe a b) = IO (Source b)
-  link source@(Source _) (FromSync pipe) = source `link` pipe
-  link source@(Source _) (AsyncPipe sink source') = do
-    source `link` sink
+  Async sink source' -> do
+    source `sourceSink` sink
     return source'
 
-  link (Merge s1 s2) pipe = do
-    s1' <- s1 `link` pipe 
-    s2' <- s2 `link` pipe
-    return $ Merge s1' s2'
+sourcePipe (Merge s1 s2) pipe = do
+  s1' <- s1 `sourcePipe` pipe 
+  s2' <- s2 `sourcePipe` pipe
+  return $ Merge s1' s2'
   
-  link EmptySource pipe = return EmptySource
-  
-instance Link (SyncPipe a b) (Sink b) where
-  type L (SyncPipe a b) (Sink b) = Sink a
-  link (SyncPipe p) (Sink s) = Sink $ \v -> p v >>= s
-  
-instance Link (AsyncPipe a b) (Sink b) where
-  type L (AsyncPipe a b) (Sink b) = IO (Sink a)
-  link (AsyncPipe sink source) sink1 = do
-    source `link` sink1
-    return sink
-  link (FromSync pipe) sink = return $ pipe `link` sink
+sourcePipe EmptySource pipe = return EmptySource
 
-instance Link (SyncPipe a b) (SyncPipe b c) where
-  type L (SyncPipe a b) (SyncPipe b c) = SyncPipe a c
-  link (SyncPipe p1) (SyncPipe p2) = SyncPipe $ \v -> p1 v >>= p2
+pipeSink :: Pipe a b -> Sink b -> IO (Sink a)
+pipeSink (Sync p) (Sink s) = return $ Sink $ \v -> p v >>= s
+pipeSink (Async sink source) sink1 = do
+  source `sourceSink` sink1
+  return sink
 
-instance Link (AsyncPipe a b) (AsyncPipe b c) where
-  type L (AsyncPipe a b) (AsyncPipe b c) = IO (AsyncPipe a c)
-  link (FromSync p1) (FromSync p2) = return $ FromSync $ p1 `link` p2
-  link (FromSync p1) (AsyncPipe sink source) = return $ AsyncPipe (p1 `link` sink) source
-  link (AsyncPipe sink source) (FromSync p) = do
-    s' <- source `link` p                                          
-    return $ AsyncPipe sink s'
-  link (AsyncPipe sink1 source1) (AsyncPipe sink2 source2) = do
-    source1 `link` sink2
-    return $ AsyncPipe sink1 source2
+pipePipe :: Pipe a b -> Pipe b c -> IO (Pipe a c)
+pipePipe (Sync p1) (Sync p2) = return $ Sync $ \v -> p1 v >>= p2
+pipePipe p1@(Sync _) (Async sink source) = do
+  sink' <- p1 `pipeSink` sink                                       
+  return $ Async sink' source
+  
+pipePipe (Async sink source) pipe@(Sync _) = do
+  s' <- source `sourcePipe` pipe                                
+  return $ Async sink s'
+
+pipePipe (Async sink1 source1) (Async sink2 source2) = do
+    source1 `sourceSink` sink2
+    return $ Async sink1 source2
+
