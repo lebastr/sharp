11ea14ac9d42b6134645a04d89c6b6b561ce4f4a
Alexander Lebedev <lebedev@smartblobs.org>
2013-05-04 11:58:54 +0000

    Type Classes and StateT was removed

--- Expr.hs
+++ Expr.hs
@@ -1,18 +1,12 @@
-{-# LANGUAGE GADTs, TypeFamilies, FlexibleInstances, ExistentialQuantification, ImpredicativeTypes, FlexibleContexts, UndecidableInstances #-}
+{-# LANGUAGE GADTs #-}
 
 module Expr where
 
-import Link
 import PrimitiveTypes
-import Data.Monoid
-import Control.Arrow
 import Data.IORef
-import System.IO.Unsafe (unsafePerformIO)
 import Map
+import Control.Monad
 import Prelude hiding (lookup)
-import Haste hiding (eval)
-import Control.Monad.State.Strict
-
 
 data App = App
 
@@ -23,233 +17,177 @@ data Expr a where
 
 -- Sink section
   SinkExpr  :: Sink a -> Expr (Sink a)
-  SPipeSink :: Expr (SyncPipe a b) -> Expr (Sink b) -> Expr (Sink a)
-  APipeSink :: Expr (AsyncPipe a b) -> Expr (Sink b) -> Expr (Sink a)
+  PipeSink :: Expr (Pipe a b) -> Expr (Sink b) -> Expr (Sink a)
   MergeSink :: Expr (Sink a) -> Expr (Sink a) -> Expr (Sink a)
-
 
 -- Source section
   SourceExpr :: Int -> Source a -> Expr (Source a)
-  SourceSPipe :: Int -> Expr (Source a) -> Expr (SyncPipe a b) -> Expr (Source b)
-  SourceAPipe :: Int -> Expr (Source a) -> Expr (AsyncPipe a b) -> Expr (Source b)
+  SourcePipe :: Int -> Expr (Source a) -> Expr (Pipe a b) -> Expr (Source b)
   MergeSource :: Int -> Expr (Source a) -> Expr (Source a) -> Expr (Source a)
   FilterSource :: Int -> (a -> Bool) -> Expr (Source a) -> Expr (Source a)
 
--- Sync Pipe section
-  SPipeExpr :: SyncPipe a b -> Expr (SyncPipe a b)
-  SPipeExprIO :: IO (SyncPipe a b) -> Expr (SyncPipe a b)
-  SSPipe :: Expr (SyncPipe a b) -> Expr (SyncPipe b c) -> Expr (SyncPipe a c)
-  FirstArrowS :: Expr (SyncPipe a b) -> Expr (SyncPipe (a,t) (b,t))
---  Accum :: (a -> t -> a) -> a -> Expr (SyncPipe t a)
-  IfThenElseSync :: (a -> Bool) -> Expr (SyncPipe a b) -> Expr (SyncPipe a b) -> Expr (SyncPipe a b)
-  Snapshot :: Expr (Source b) -> b -> Expr (SyncPipe a b)
-
--- Async Pipe section
-  APipeExprIO :: IO (AsyncPipe a b) -> Expr (AsyncPipe a b)
-  AAPipe :: Expr (AsyncPipe a b) -> Expr (AsyncPipe b c) -> Expr (AsyncPipe a c)
-  FirstArrowA :: Expr (AsyncPipe a b) -> Expr (AsyncPipe (a,t) (b,t))
-  ToAsync :: Expr (SyncPipe a b) -> Expr (AsyncPipe a b)
-  IfThenElseAsync :: (a -> Bool) -> Expr (AsyncPipe a b) -> Expr (AsyncPipe a b) -> Expr (AsyncPipe a b)
+-- Pipe section
+  PipeExpr :: IO (Pipe a b) -> Expr (Pipe a b)
+  PipePipe :: Expr (Pipe a b) -> Expr (Pipe b c) -> Expr (Pipe a c)
+  FirstArrowA :: Expr (Pipe a b) -> Expr (Pipe (a,t) (b,t))
+  IfThenElse :: (a -> Bool) -> Expr (Pipe a b) -> Expr (Pipe a b) -> Expr (Pipe a b)
+  Snapshot :: Expr (Source b) -> b -> Expr (Pipe a b)
   
+-- class Draw a where
+--   draw :: a -> String
 
-class Draw a where
-  draw :: a -> String
+-- instance Draw String where
+--   draw s = "(" ++ show s ++ ")"
 
-instance Draw String where
-  draw s = "(" ++ show s ++ ")"
+-- instance Draw Int where
+--   draw s = "(" ++ show s ++ ")"
 
-instance Draw Int where
-  draw s = "(" ++ show s ++ ")"
-
-instance (Draw a, Draw b) => Draw (a,b) where  
-  draw (a,b) = "(" ++ draw a ++ " " ++ draw b ++ ")"
+-- instance (Draw a, Draw b) => Draw (a,b) where  
+--   draw (a,b) = "(" ++ draw a ++ " " ++ draw b ++ ")"
   
-instance (Draw a, Draw b, Draw c) => Draw (a,b,c) where  
-  draw (a,b,c) = "(" ++ draw a ++ " " ++ draw b ++ " " ++ draw c ++ ")"
+-- instance (Draw a, Draw b, Draw c) => Draw (a,b,c) where  
+--   draw (a,b,c) = "(" ++ draw a ++ " " ++ draw b ++ " " ++ draw c ++ ")"
 
-instance (Draw a, Draw b, Draw c, Draw d) => Draw (a,b,c,d) where  
-  draw (a,b,c,d) = "(" ++ draw a ++ " " ++ draw b ++ " " ++ draw c ++ " " ++ draw d ++ ")"
+-- instance (Draw a, Draw b, Draw c, Draw d) => Draw (a,b,c,d) where  
+--   draw (a,b,c,d) = "(" ++ draw a ++ " " ++ draw b ++ " " ++ draw c ++ " " ++ draw d ++ ")"
 
-instance Draw (Expr a) where
-  draw (Parallel app1 app2) = draw ("||", app1, app2)
-  draw (SourceSink source sink) = draw (">-->", source, sink)
+-- instance Draw (Expr a) where
+--   draw (Parallel app1 app2) = draw ("||", app1, app2)
+--   draw (SourceSink source sink) = draw (">-->", source, sink)
 
-  draw (SinkExpr _) = "sink"
-  draw (SPipeSink pipe sink) = draw ("-->", pipe, sink)
-  draw (APipeSink pipe sink) = draw ("-->", pipe, sink)
-  draw (MergeSink s1 s2) = draw ("+", s1, s2)
+--   draw (SinkExpr _) = "sink"
+--   draw (SPipeSink pipe sink) = draw ("-->", pipe, sink)
+--   draw (APipeSink pipe sink) = draw ("-->", pipe, sink)
+--   draw (MergeSink s1 s2) = draw ("+", s1, s2)
 
-  draw (SourceExpr i s) = draw (i, "source")
-  draw (SourceSPipe i s p) = draw (i, (">--", s, p))
-  draw (SourceAPipe i s p) = draw (i, (">--", s, p))
-  draw (MergeSource i s1 s2) = draw (i, ("+", s1, s2))
-  draw (FilterSource i p s) = draw (i, ("filter", s))
+--   draw (SourceExpr i s) = draw (i, "source")
+--   draw (SourceSPipe i s p) = draw (i, (">--", s, p))
+--   draw (SourceAPipe i s p) = draw (i, (">--", s, p))
+--   draw (MergeSource i s1 s2) = draw (i, ("+", s1, s2))
+--   draw (FilterSource i p s) = draw (i, ("filter", s))
 
-  draw (SPipeExpr _) = "pipe"
-  draw (SPipeExprIO _) = "pipeIO"
-  draw (SSPipe p1 p2) = draw (">>>", p1, p2)
-  draw (FirstArrowS p) = draw ("first", p)
-  draw (IfThenElseSync _ p1 p2) = draw ("ifThenElse", p1, p2)
-  draw (Snapshot s _) = draw ("snapshot", s)
+--   draw (SPipeExpr _) = "pipe"
+--   draw (SPipeExprIO _) = "pipeIO"
+--   draw (SSPipe p1 p2) = draw (">>>", p1, p2)
+--   draw (FirstArrowS p) = draw ("first", p)
+--   draw (IfThenElseSync _ p1 p2) = draw ("ifThenElse", p1, p2)
+--   draw (Snapshot s _) = draw ("snapshot", s)
 
-  draw (APipeExprIO _) = "apipeIO"
-  draw (AAPipe p1 p2) = draw (">>>", p1, p2)
-  draw (FirstArrowA p) = draw ("first", p)
-  draw (ToAsync p) = draw ("toAsync", p)
-  draw (IfThenElseAsync _ p1 p2) = draw ("ifThenElse", p1, p2)
+--   draw (APipeExprIO _) = "apipeIO"
+--   draw (AAPipe p1 p2) = draw (">>>", p1, p2)
+--   draw (FirstArrowA p) = draw ("first", p)
+--   draw (ToAsync p) = draw ("toAsync", p)
+--   draw (IfThenElseAsync _ p1 p2) = draw ("ifThenElse", p1, p2)
 
-reify = unsafePerformIO $ newIORef (0 :: Int)
+-- reify = unsafePerformIO $ newIORef (0 :: Int)
 
-refId = modifyIORef reify (+1) >> readIORef reify
+-- refId = modifyIORef reify (+1) >> readIORef reify
 
-fromEvalM :: EvalM a -> a
-fromEvalM = undefined
-
-memoize :: Int -> EvalM (Source a) -> EvalM (Source a)
-memoize p action = do
-  m <- get
-  case lookup p m (fromEvalM action) of
+memo :: IORef Map -> Int -> IO a -> IO a
+memo ref p action = do
+  m <- readIORef ref
+  case lookup p m undefined of
     Nothing -> do
       s <- action
-      modify (insert p s)
+      modifyIORef ref (insert p s)
       return s
     Just value -> do
       return value
 
-type EvalM a = StateT Map (IO) a
-
-class Eval a where
-  type E a :: *
-  eval :: a -> EvalM (E a)
+evalApp :: Expr App -> IO ()
+evalApp (Parallel app1 app2) = evalApp app1 >> evalApp app2
+evalApp (SourceSink source sink) = do
+  state <- newIORef empty
+  a <- evalSource state source 
+  b <- evalSink state sink
+  sourceSink a b
+    
+evalSource :: IORef Map -> Expr (Source a) -> IO (Source a)
+evalSource state (SourceExpr rId s) = memo state rId $ return s
+evalSource state (SourcePipe rId sExpr pExpr) = memo state rId $ do
+  s <- evalSource state sExpr
+  p <- evalPipe state pExpr
+  sourcePipe s p
   
-instance Eval (Expr App) where
-  type E (Expr App) = ()
-  eval (Parallel app1 app2) = eval app1 >> eval app2
-  eval (SourceSink source sink) = do
-    a <- eval source 
-    b <- eval sink
-    lift $ a `link` b
+evalSource state (MergeSource rId sExpr1 sExpr2) = memo state rId $ do
+  s1 <- evalSource state sExpr1
+  s2 <- evalSource state sExpr2
+  return $ s1 `Merge` s2
     
-instance Eval (Expr (Source a)) where
-  type E (Expr (Source a)) = Source a
-  eval (SourceExpr rId s) = memoize rId $ return s
+evalSource state (FilterSource rId p sExpr) = memo state rId $ do
+  s <- evalSource state sExpr
+  filterSource p s
 
-  eval (SourceSPipe rId sExpr pExpr) = memoize rId $ do
-    s <- eval sExpr
-    p <- eval pExpr
-    lift $ s `link` p
-  
-  eval (SourceAPipe rId sExpr pExpr) = memoize rId $ do
-    s <- eval sExpr
-    p <- eval pExpr
-    lift $ s `link` p
-  
-  eval (MergeSource rId sExpr1 sExpr2) = memoize rId $ do
-    s1 <- eval sExpr1
-    s2 <- eval sExpr2
-    return $ s1 `Merge` s2
+evalSink :: IORef Map -> Expr (Sink a) -> IO (Sink a)
+evalSink _ (SinkExpr s) = return s
+evalSink state (PipeSink pExpr sExpr) = do
+  p <- evalPipe state pExpr
+  s <- evalSink state sExpr
+  pipeSink p s
     
-  eval (FilterSource rId p sExpr) = memoize rId $ do
-    s <- eval sExpr
-    lift $ filterSource p s
+evalSink state (MergeSink sExpr1 sExpr2) = do
+  s1 <- evalSink state sExpr1
+  s2 <- evalSink state sExpr2
+  return $ mergeSink s1 s2
 
-instance Eval (Expr (Sink a)) where
-  type E (Expr (Sink a)) = Sink a
-  eval (SinkExpr s) = return s
-  eval (SPipeSink pExpr sExpr) = do
-    p <- eval pExpr
-    s <- eval sExpr
-    return $ p `link` s
-    
-  eval (APipeSink pExpr sExpr) = do
-    p <- eval pExpr
-    s <- eval sExpr
-    lift $ p `link` s
-    
-  eval (MergeSink sExpr1 sExpr2) = do
-    s1 <- eval sExpr1
-    s2 <- eval sExpr2
-    return $ s1 `mappend` s2
+evalPipe :: IORef Map -> Expr (Pipe a b) -> IO (Pipe a b)
+evalPipe _ (PipeExpr p) = p
+evalPipe state (PipePipe pExpr1 pExpr2) = do
+  p1 <- evalPipe state pExpr1
+  p2 <- evalPipe state pExpr2
+  pipePipe p1 p2
 
-instance Eval (Expr (SyncPipe a b)) where
-  type E (Expr (SyncPipe a b)) = SyncPipe a b
-  eval (SPipeExpr p) = return p
-  eval (SPipeExprIO p) = lift p
-  eval (SSPipe pExpr1 pExpr2) = do
-    p1 <- eval pExpr1
-    p2 <- eval pExpr2
-    return $ p1 `link` p2
+evalPipe state (Snapshot sExpr v0) = do
+  ref <- newIORef v0
+  source <- evalSource state sExpr
+  sourceSink source $ Sink $ writeIORef ref
+  return $ Sync $ \_ -> readIORef ref
 
-  eval (FirstArrowS pExpr) = do
-    p <- eval pExpr
-    return $ first p
-
-  -- eval (Accum g v0) = do
-  --   ref <- lift $ newIORef v0
-  --   return $ SyncPipe $ \x -> do
-  --     v <- readIORef ref
-  --     writeIORef ref (g v x)
-  --     return v
-
-  eval (Snapshot sExpr v0) = do
-    ref <- lift $ newIORef v0
-    source <- eval sExpr
-    lift $ source `link` (Sink $ writeIORef ref)
-    return $ SyncPipe $ \_ -> readIORef ref
-
-  eval (IfThenElseSync p pExpr1 pExpr2) = do
-    pipe1 <- eval pExpr1
-    pipe2 <- eval pExpr2
-    return $ SyncPipe $ \v -> case p v of
-      True -> runSyncPipe pipe1 v
-      False -> runSyncPipe pipe2 v
-
-instance Eval (Expr (AsyncPipe a b)) where
-  type E (Expr (AsyncPipe a b)) = AsyncPipe a b
-  eval (APipeExprIO p) = lift p
-  eval (AAPipe pExpr1 pExpr2) = do
-    p1 <- eval pExpr1
-    p2 <- eval pExpr2
-    lift $ p1 `link` p2
-    
-  eval (FirstArrowA pExpr) = do
-    pipe <- eval pExpr
-    case pipe of 
-      FromSync p -> return $ FromSync $ first p
-      AsyncPipe sink source -> do
-        ref <- lift $ newIORef undefined
-        let sink' = Sink $ \(v,t) -> do
-              writeIORef ref t
-              runSink sink v
+evalPipe state (FirstArrowA pExpr) = do
+  pipe <- evalPipe state pExpr
+  case pipe of 
+    Sync p -> return $ Sync $ \(v,t) -> do
+      v' <- p v
+      return (v',t)
+      
+    Async sink source -> do
+      ref <- newIORef undefined
+      let sink' = Sink $ \(v,t) -> do
+            writeIORef ref t
+            runSink sink v
             
-            pipe' = SyncPipe $ \v -> do
-              t <- readIORef ref
-              return (v,t)
+          pipe' = Sync $ \v -> do
+            t <- readIORef ref
+            return (v,t)
               
-        source' <- lift $ (link :: Source a -> SyncPipe a b -> IO (Source b)) source pipe'
-        return $ AsyncPipe sink' source'
+      source' <- sourcePipe source pipe'
+      return $ Async sink' source'
     
-  eval (ToAsync pExpr) = do
-    p <- eval pExpr
-    return $ FromSync p
-    
-  eval (IfThenElseAsync p pExpr1 pExpr2) = do
-    pipe1 <- eval pExpr1
-    pipe2 <- eval pExpr2
-    ref1 <- lift $ newIORef []
-    ref2 <- lift $ newIORef []
-    let sink = Sink $ \v -> case p v of
-          True -> do
-            subs <- readIORef ref1
-            forM_ subs $ \c -> runSink c v          
-          False -> do
-            subs <- readIORef ref2
-            forM_ subs $ \c -> runSink c v
+evalPipe state (IfThenElse p pExpr1 pExpr2) = do
+  pipe1 <- evalPipe state pExpr1
+  pipe2 <- evalPipe state pExpr2
+  evalPipe' p pipe1 pipe2
+  where
+    evalPipe' p (Sync runPipe1) (Sync runPipe2) = do 
+      return $ Sync $ \v -> case p v of
+        True -> runPipe1 v
+        False -> runPipe2 v
+
+    evalPipe' p pipe1 pipe2 = do
+      ref1 <- newIORef []
+      ref2 <- newIORef []
+      let sink = Sink $ \v -> case p v of
+            True -> do
+              subs <- readIORef ref1
+              forM_ subs $ \c -> runSink c v          
+            False -> do
+              subs <- readIORef ref2
+              forM_ subs $ \c -> runSink c v
             
-        source1 = Source ref1
-        source2 = Source ref2
+          source1 = Source ref1
+          source2 = Source ref2
     
-    source1' <- lift $ source1 `link` pipe1
-    source2' <- lift $ source2 `link` pipe2
+      source1' <- sourcePipe source1 pipe1
+      source2' <- sourcePipe source2 pipe2
     
-    return $ AsyncPipe sink $ Merge source1' source2'
+      return $ Async sink $ Merge source1' source2'
