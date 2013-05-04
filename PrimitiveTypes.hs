19cb649c3269041bdd0d0dc69f7bf126a964b243
Dmitry Vyal <akamaus@gmail.com>
2013-05-04 12:09:08 +0400

    whitespaces removed in PrimitiveTypes.hs

--- PrimitiveTypes.hs
+++ PrimitiveTypes.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, TypeFamilies, GeneralizedNewtypeDeriving, DeriveDataTypeable,  
-    GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses, TypeFamilies, GeneralizedNewtypeDeriving, DeriveDataTypeable, GADTs #-}
 
 module PrimitiveTypes where
 
@@ -22,17 +21,17 @@ createSource call = do
 -- cAsyncPipe :: ((a -> IO ()) -> IO ()) -> IO (Source a)
 -- cAsyncPipe call = do
 --   ref <- newIORef []
---   let sink = Sink $ \v -> call $ \v' -> 
+--   let sink = Sink $ \v -> call $ \v' ->
 
 -- delay :: Int -> IO (AsyncPipe a a)
 -- delay dt = do
 --   ref <- newIORef []
 --   let sink = Sink $ \v -> do
---         subs <- readIORef ref                    
+--         subs <- readIORef ref
 --         setTimeout dt $ forM_ subs $ \c -> runSink c v
 --       source = Source ref
 --   return $ Async sink source
-  
+
 filterSource :: (a -> Bool) -> Source a -> IO (Source a)
 filterSource p source = do
   ref <- newIORef []
@@ -52,14 +51,14 @@ data Pipe a b = Async (Sink a) (Source b)
 -- instance Category SyncPipe where
 --   id = SyncPipe $ runKleisli id
 --   (.) = flip link
-  
+
 -- instance Arrow SyncPipe where
 --   arr = SyncPipe . runKleisli . arr
 --   first = SyncPipe . runKleisli . first . Kleisli . runSyncPipe
 
 -- instance Functor (SyncPipe t) where
 --   fmap f p = arr f . p
-  
+
 -- instance Applicative (SyncPipe t) where
 --   pure = arr . const
 --   (<*>) fp vp = arr (\(f,v) -> f v) . (fp &&& vp)
@@ -67,11 +66,11 @@ data Pipe a b = Async (Sink a) (Source b)
 -- instance Monoid a => Monoid (SyncPipe t a) where
 --   mempty = arr $ const mempty
 --   mappend p1 p2 = mappend <$> p1 <*> p2
-  
+
 -- instance Monoid (Sink a) where
 --   mempty = Sink $ \_ -> return ()
 --   mappend (Sink s1) (Sink s2) = Sink $ \v -> s1 v >> s2 v
-  
+
 sourceSink :: Source a -> Sink a -> IO ()
 sourceSink (Source ref) sink = modifyIORef ref (sink:)
 sourceSink (Merge s1 s2) sink = s1 `sourceSink` sink >> s2 `sourceSink` sink
@@ -88,7 +87,7 @@ sourcePipe source@(Source ref) pipe = case pipe of
             False -> do
               v' <- runPipe v
               forM_ subs $ \c -> runSink c v'
-    
+
     modifyIORef ref (sink:)
     return $ Source ref1
 
@@ -97,10 +96,10 @@ sourcePipe source@(Source ref) pipe = case pipe of
     return source'
 
 sourcePipe (Merge s1 s2) pipe = do
-  s1' <- s1 `sourcePipe` pipe 
+  s1' <- s1 `sourcePipe` pipe
   s2' <- s2 `sourcePipe` pipe
   return $ Merge s1' s2'
-  
+
 sourcePipe EmptySource pipe = return EmptySource
 
 pipeSink :: Pipe a b -> Sink b -> IO (Sink a)
@@ -112,11 +111,11 @@ pipeSink (Async sink source) sink1 = do
 pipePipe :: Pipe a b -> Pipe b c -> IO (Pipe a c)
 pipePipe (Sync p1) (Sync p2) = return $ Sync $ \v -> p1 v >>= p2
 pipePipe p1@(Sync _) (Async sink source) = do
-  sink' <- p1 `pipeSink` sink                                       
+  sink' <- p1 `pipeSink` sink
   return $ Async sink' source
-  
+
 pipePipe (Async sink source) pipe@(Sync _) = do
-  s' <- source `sourcePipe` pipe                                
+  s' <- source `sourcePipe` pipe
   return $ Async sink s'
 
 pipePipe (Async sink1 source1) (Async sink2 source2) = do
