ba6480bf38dc542a7882e25733bb048b7add9d20
Alexander Lebedev <lebedev@smartblobs.org>
2013-05-24 22:44:10 +0000

    Pipes are Kleisli (ContT () IO) a b

--- src/Sharp1/PrimitiveFrp.hs
+++ src/Sharp1/PrimitiveFrp.hs
@@ -7,27 +7,35 @@ import Control.Monad.Cont
 import Data.IORef
 import Control.Monad.STM
 import Control.Concurrent.STM.TVar
+import Control.Applicative
+import Data.Maybe
+import Control.Concurrent
+import Control.Concurrent.MVar
+
+type Time = Int
 
 --------------------Pipe type--------------------
 
-data Pipe a b = Sync (Kleisli (IO) a b)
-              | Async (Kleisli (ContT () (IO)) a b)
+type Pipe a b = Kleisli (ContT () (IO)) a b
 
 toAsync :: Pipe a b -> Pipe a b
-toAsync p@(Async _) = p
-toAsync (Sync (Kleisli e)) = Async $ Kleisli $ lift . e
+toAsync (Kleisli pipe) = Kleisli $ \v -> ContT $ \cps -> do
+  forkIO $ runContT (pipe v) cps
+  return ()
 
-instance Category Pipe where
-  id = Sync id
-  (.) (Sync p0) (Sync p1) = Sync $ p0 . p1
-  (.) (Async p0) (Async p1) = Async $ p0 . p1
-  (.) p0@(Sync _) p1@(Async _) = toAsync p0 . p1
-  (.) p0@(Async _) p1@(Sync _) = p0 . toAsync p1
+toSync :: Pipe a b -> IO (Pipe a b)
+toSync (Kleisli pipe) = do
+  mvar <- newEmptyMVar
+  return $ Kleisli $ \v -> do
+    lift $ putMVar mvar ()
+    v' <- pipe v
+    lift $ takeMVar mvar
+    return v'
 
-instance Arrow Pipe where
-  arr f = Sync $ arr f
-  first (Sync a) = Sync $ first a
-  first (Async a) = Async $ first a
+delay :: Time -> Pipe a a
+delay dt = Kleisli $ \v -> ContT $ \cps -> do
+  forkIO $ threadDelay dt >> cps v
+  return ()
 
 ----------------------- Source type -------------------
 
@@ -44,8 +52,7 @@ clojureSources sources = do
   return $ ContT $ \cps -> modifyIORef ref (++[cps])
 
 sourcePipe :: Source a -> Pipe a b -> IO (Source b)
-sourcePipe s p@(Sync _) = sourcePipe s $ toAsync p
-sourcePipe s (Async (Kleisli p)) = clojureSources $ [s >>= p]
+sourcePipe s (Kleisli p) = clojureSources $ [s >>= p]
 
 mergeSource :: Source a -> Source a -> IO (Source a)
 mergeSource s1 s2 = clojureSources [s1,s2]
@@ -54,7 +61,7 @@ snapshot :: Source a -> a -> IO (Pipe t a)
 snapshot source value = do
   ref <- newTVarIO value
   runContT source $ \v -> atomically $ writeTVar ref v
-  return $ Sync $ Kleisli $ const $ atomically $ readTVar ref
+  return $ Kleisli $ const $ lift $ atomically $ readTVar ref
 
 createSource :: IO (Sink a, Source a)
 createSource = do
@@ -65,6 +72,23 @@ createSource = do
 
       source = ContT $ \cps -> modifyIORef ref (++[cps])
   return (sink, source)
+
+filterSource :: (a -> Bool) -> Source a -> Source a
+filterSource p s = ContT $ \cps -> runContT s $ \v -> case p v of
+  False -> return ()
+  True -> cps v
+
+appSource :: Source (a -> b) -> Source a -> IO (Source b)
+appSource fs vs = do
+  fs' <- sourcePipe fs $ arr Just
+  vs' <- sourcePipe vs $ arr Just
+  fs'' <- sourcePipe fs $ arr $ \_ -> ()
+  vs'' <- sourcePipe vs $ arr $ \_ -> ()
+  pf <- snapshot fs' Nothing
+  pv <- snapshot vs' Nothing
+  source <- mergeSource fs'' vs''
+  source' <- source `sourcePipe` ((pf &&& pv) >>> arr (\(f,v) -> f <*> v))
+  (filterSource isJust source') `sourcePipe` (arr fromJust)
 
 -------------------------- Sink type ---------------------------
 
@@ -77,5 +101,4 @@ mergeSink :: Sink a -> Sink a -> Sink a
 mergeSink s1 s2 = \s -> s1 s >> s2 s
 
 pipeSink :: Pipe a b -> Sink b -> Sink a
-pipeSink (Sync (Kleisli p)) s = \v -> p v >>= s
-pipeSink (Async (Kleisli p)) s = \v -> runContT (p v) s
+pipeSink (Kleisli p) s = \v -> runContT (p v) s
